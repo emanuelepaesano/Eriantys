6e791cc62ab3053ad3ba45330192fe9aa9ef599c
emanu <emanuele.paesano@mail.polimi.it>
2022-04-11 17:00:48 +0200

    added AssistantEnum

--- src/main/java/it/polimi/ingsw/model/Assistant.java
+++ src/main/java/it/polimi/ingsw/model/Assistant.java
@@ -1,7 +1,7 @@
 package it.polimi.ingsw.model;
 
-// TODO: 11/04/2022 I think this can be an enum, with the 1...10 assistants and that's it.
-//  the player can then have a map <assistant,bool> to show the asssistants
+//I think this can be an enum, with the 1...10 assistants and that's it.
+//the player can then have a map <assistant,bool> to show the asssistants
 
 public class Assistant {
     Integer speed;
