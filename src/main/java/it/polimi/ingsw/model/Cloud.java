c4c52aaccf997b45b9ed1554f49e7ac95c020a36
Kodai <30628299+kodai-t@users.noreply.github.com>
2022-04-15 18:21:33 +0200

    set default clouds

--- src/main/java/it/polimi/ingsw/model/Cloud.java
+++ src/main/java/it/polimi/ingsw/model/Cloud.java
@@ -1,12 +1,25 @@
 package it.polimi.ingsw.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class Cloud {
     private Map<StudColor, Integer> students;
 
-    public Cloud(Map<StudColor, Integer> students){
-        this.students = students;
+    public Cloud(){
+        this.students = getDefaultStudents();;
+    }
+
+    /**
+     *
+     * @return Map of all studColors to 0
+     */
+    private Map<StudColor,Integer> getDefaultStudents(){
+        students = new HashMap<>();
+        for (StudColor sc: StudColor.values()){
+            students.put(sc,0);
+        }
+        return students;
     }
 
 }
