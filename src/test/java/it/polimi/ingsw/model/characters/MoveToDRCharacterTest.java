afc23d3c4dcd8a401359fe8b38949bc8ba0b420a
Kodai <30628299+kodai-t@users.noreply.github.com>
2022-06-30 18:42:53 +0200

    deleted unused imports

--- src/test/java/it/polimi/ingsw/model/characters/MoveToDRCharacterTest.java
+++ src/test/java/it/polimi/ingsw/model/characters/MoveToDRCharacterTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MoveToDRCharacterTest {
 
